c4fa644d9584207223ee29bd5ad0cc7436ba7fed
Ueli Reber <26281183+ureber@users.noreply.github.com>
2018-06-06 10:12:31 +0200

    Add files via upload
    Some necessary changes due to changed website design.

--- vegibot.R
+++ vegibot.R
@@ -1,6 +1,8 @@
-##########################
-# Twitterbot @VegivonRou #
-##########################
+####################
+# Vegibot @ vonRou #
+####################
+
+#inspired by this tutorial: http://www.r-datacollection.com/blog/Programming-a-Twitter-bot/
 
 library(rvest)
 library(stringr)
@@ -13,35 +15,25 @@ menu <- read_html(paste0("http://zfv.ch/de/microsites/mensa-und-cafeteria-vonrol
 menu.today <- menu %>%
   html_nodes(paste0("[data-date='", Sys.Date(), "']"))
 
-#select the vegi menu
-menu.vegi <- html_text(menu.today[2])
-
-#extract the pacman
-pacman.str <- as.character(html_children(menu.today[2])[1])
-
-if (str_detect(pacman.str, "green") == TRUE) {
-  pacman <- 1
-  } else {
-    if (str_detect(pacman.str, "yellow") == TRUE) {
-      pacman <- 2
-      } else {
-        pacman <- 3
-      }
-    }
-
 #check if menu is vegan
-if (str_detect(tolower(menu.vegi), "vegan") == TRUE) {
+if (str_detect(as.character(html_children(menu.today[2])[1]), "vegan") == TRUE) {
   vegan <- 1
 } else {
   vegan <- 0
 }
 
+#select the vegi menu
+menu.vegi <- html_text(menu.today[2])
+
 #trim menu
-menu.vegi <- str_replace_all(menu.vegi, "\n", " ")
-menu.vegi <- str_replace_all(menu.vegi, "«natürlich vegi»", " ")
-menu.vegi <- str_replace_all(menu.vegi, "CHF 6.60 / 12.60 | CHF 6.60 / CHF 12.60", " ")
-menu.vegi <- str_replace(menu.vegi, "[^\\s]*(VEGAN)[^\\s]*", "") 
+menu.vegi <- str_replace(menu.vegi, "\nmit", " mit")
+menu.vegi <- str_replace(menu.vegi, "\n&", " &")
+menu.vegi <- str_replace_all(menu.vegi, "(?<=[:alpha:])\\n(?=[:alpha:])", ", ")
+menu.vegi <- str_replace_all(menu.vegi, "\n", "")
+menu.vegi <- str_replace_all(menu.vegi, "natürlich vegi", "")
+menu.vegi <- str_replace_all(menu.vegi, "\\CHF(.*)", " ")
 menu.vegi <- str_trim(menu.vegi, "both")
+menu.vegi <- str_replace_all(menu.vegi, "  ", " ")
 
 #assemble the tweet
 if (str_detect(tolower(menu.vegi), "geschlossen") == TRUE) {
@@ -51,43 +43,21 @@ if (str_detect(tolower(menu.vegi), "geschlossen") == TRUE) {
       text <- "Hüt gits ke Vegimenü. Muesch äuä säuber choche. 🍳"
       } else {
         if (vegan == 1){
-          if (pacman == 1){
-            text <- paste0("Hüt gits öppis fürd Gsündi: ", menu.vegi, ". Vou vegan! E Guete! 💚")
-          } else {
-            if (pacman == 2){
-              text <- paste0("Hüt gits öppis füre Geist: ", menu.vegi, ". Vou vegan! E Guete! 💚")
-            } else {
-              text <- paste0("Hüt gits öppis fürd Lust: ", menu.vegi, ". Vou vegan! E Guete! 💚")
-            }
-          }
+            text <- paste0("Hüt gits: ", menu.vegi, ". Vou vegan! E Guete! 💚")
         } else {
-          if (pacman == 1){
-            text <- paste0("Hüt gits öppis fürd Gsündi: ", menu.vegi, ". E Guete!")
-          } else {
-            if (pacman == 2){
-              text <- paste0("Hüt gits öppis füre Geist: ", menu.vegi, ". E Guete!")
-            } else {
-              text <- paste0("Hüt gits öppis fürd Lust: ", menu.vegi, ". E Guete!")
-            }
-          }
+          text <- paste0("Hüt gits: ", menu.vegi, ". E Guete!")
         }
       }
-  }
+    }
 
-#set up Twitter access for user @VegivonRou
-#the actual credentials have been removed to make sure that only the real Vegibot tweets
-consumer_key <- 
-consumer_secret <- 
-access_token <- 
-access_secret <- 
-
+#set up Twitter access for user VegivonRou
+source("credentials.R", encoding = "UTF-8")
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
 #send Tweet
 tweet(text)
 
 #save today's menu to the log file
-log.df <- data.frame(date = Sys.Date(), vegan = vegan, pacman = pacman, menu = menu.vegi, tweet = text)
+log.df <- data.frame(date = Sys.Date(), vegan = vegan, pacman = NA, menu = menu.vegi, tweet = text)
 
-write.table(log.df, file = "menu_log.csv", row.names = FALSE, col.names = FALSE,
-            append = TRUE, sep = ";")
+write.table(log.df, file = "menu_log.csv", row.names = FALSE, col.names = FALSE, append = TRUE, sep = ";")
